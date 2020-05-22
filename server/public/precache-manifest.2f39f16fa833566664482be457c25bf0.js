0140495f6c5b3c1a7fc9a936eeb54b5a4f8b4657
Madalin <madalin.lingurar@gmail.com>
2020-05-22 06:25:54 +0200

    I hope is last deploy

---
+++ server/public/precache-manifest.2f39f16fa833566664482be457c25bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c8de94fc26098bc6fd92b418d641cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae7fde83aa524d2ebac9",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f72a6002dadaea37b67a",
+    "url": "/static/css/main.04466371.chunk.css"
+  },
+  {
+    "revision": "ae7fde83aa524d2ebac9",
+    "url": "/static/js/2.e9fef7e1.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/static/js/2.e9fef7e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72a6002dadaea37b67a",
+    "url": "/static/js/main.012e44ab.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
